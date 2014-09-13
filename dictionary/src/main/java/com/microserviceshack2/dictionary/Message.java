1cb2801ed65a0eb83ad380b622f5e61d8e43cc61
Ivan Mani <ivan.mani@icloud.com>
2014-09-13 17:03:22 +0100

    Receiver

--- dictionary/src/main/java/com/microserviceshack2/dictionary/Message.java
+++ dictionary/src/main/java/com/microserviceshack2/dictionary/Message.java
@@ -1,8 +1,11 @@
 package com.microserviceshack2.dictionary;
 
+import java.io.IOException;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Message {
@@ -21,4 +24,23 @@ public class Message {
 		return mapper.writeValueAsString(details);
 	}
 
+	@SuppressWarnings("unchecked")
+	public void setFromJson(String json) throws JsonParseException,
+			JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		this.details = mapper.readValue(json, Map.class);
+
+	}
+
+	public Object get(String key) {
+		return details.get(key);
+	}
+
+	public void dump() {
+		for (Object key : details.keySet()) {
+			System.out.println("Key is " + key + " value is "
+					+ details.get(key));
+		}
+
+	}
 }
